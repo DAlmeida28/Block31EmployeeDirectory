ff51178e8b3b858f06f3395cc9c420d81b62c522
Daniel Almeida <almeida.danny@gmail.com>
2025-02-17 16:15:53 -0600

    add initial employee list. Pushing to make fetch requests from live server.

--- Employee-directory/src/App.jsx
+++ Employee-directory/src/App.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react"
 
 function App() {
 
+  useEffect(() => {
+    const listEmployees = async () => {
+      const getEmployees = await fetch('localhost:3000/employees')
+      const response = await getEmployees.json();
+      console.log(response);
+    }
+    listEmployees();
+  })
+
   return (
     <>
-     <h1>TEST</h1>
+     
     </>
   )
 }
